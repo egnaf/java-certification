b64a0bfc7c277bc34b80aaef78306132add48e75
egnaf <egnaf@yahoo.com>
2020-03-08 21:49:11 +0300

    Add variable naming rules

---
+++ oca/data_types/VariableNaming.java
@@ -0,0 +1,21 @@
+package data_types;
+
+public class VariableNaming {
+
+    public static void main(String[] args) {
+
+        /*
+            1. The variable name must start with: _, $ or letters.
+         */
+        String name;
+        String $name;
+        String _name;
+
+        /*
+            2. Other characters may be: _, $, letters, numbers.
+        */
+        String name1;
+        String first_name;
+        String $last_$name;
+    }
+}
