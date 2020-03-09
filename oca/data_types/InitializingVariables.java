7757c0a94f3509ad4796a0a76bf03d7c6baf4358
Sherzod Mamadaliev <egnaf@yahoo.com>
2020-03-09 20:03:12 +0300

    [OCA-2] Add data types specification
    * Add the byte data type specification
    * Add variable naming rules
    * Fix variable naming and add rules of number systems
    * Add primitive data types
    * Delete byte data type examples
    * Fix comment view
    * Fix value of variable, comment descriptions and style
    * Delete output the variable
    * Add examples and fix primitive types
    * Fix suppress warnings

---
+++ oca/data_types/InitializingVariables.java
@@ -0,0 +1,42 @@
+package data_types;
+
+public class InitializingVariables {
+
+    static String var = "It's a static variable.";
+
+    @SuppressWarnings({"unused", "ConstantConditions", "UnusedAssignment"})
+    public static void main(String[] args) {
+
+        /* -------------------------------------------------------------------------------
+         * Initializing variables:
+         * 1. Local variables must be initialised before use.
+         * 2. Class and instance variables are given a default value.
+         */
+        boolean var1 = false;
+        byte var2    = 0;
+        short var3   = 0;
+        int var4     = 0;
+        long var5    = 0L;
+        char var6    = '\u0000';
+        float var7   = 0.0f;
+        double var8  = 0.0;
+
+        /* -------------------------------------------------------------------------------
+         * Local variables can NOT be initialised within conditional code
+         * as there is a chance that they won't be initialised.
+         */
+        int someNumber;
+        String today = "Wednesday";
+
+        if (today.equals("Thursday")) {
+            someNumber = 1;
+        }
+        // System.out.println(someNumber); will cause error
+
+        /* -------------------------------------------------------------------------------
+         * Static variables are initialized as soon as the class is created
+         * and last the entire program.
+         */
+        System.out.println(InitializingVariables.var);
+    }
+}
---
+++ oca/data_types/VariableNaming.java
@@ -0,0 +1,23 @@
+package data_types;
+
+public class VariableNaming {
+
+    @SuppressWarnings({"unused", "NonAsciiCharacters"})
+    public static void main(String[] args) {
+
+        /* -------------------------------------------------------------------------------
+         * 1. The variable name must start with: _, $ or letters.
+         */
+        String name;
+        String $name;
+        String _name;
+
+        /* -------------------------------------------------------------------------------
+         * 2. Other characters may be: _, $, letters, numbers.
+         */
+        String name1;
+        String first_name;
+        String last_$$_name;
+        String имя_фамилия; // Non Ascii Characters also allowed
+    }
+}
